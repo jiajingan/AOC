9ab35fa09de5ce5123736703cb5091e054c216e1
Jackie Gan <tuj90887@temple.edu>
2022-12-02 00:19:08 -0500

    day 1 finished

---
+++ adventofcode22/python/d1/main.py
@@ -0,0 +1,21 @@
+# AOC day 1 in python
+
+# parse file
+fname = "adventofcode22/input.txt"
+with open(fname) as f:
+    lines = []
+    for line in f:
+        lines.append(line.strip("\n"))
+        
+maxCalories = 0
+currCal = 0
+for i in lines:
+    if (i != ''):
+        currCal += int(i)
+    else:
+        currCal = 0
+    
+    if(maxCalories<currCal):
+        maxCalories = currCal
+
+print(maxCalories)
